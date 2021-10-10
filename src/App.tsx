0796ba1140331ee3fff52a1f4588b5834c75fe87
youthfulhps <ybh942002@gmail.com>
2021-10-10 23:52:44 +0900

    Implemented test api for GET coins info

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect } from "react";
 import "./App.css";
 import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { API } from "aws-amplify";
 
 function App() {
+  useEffect(() => {
+    const getCoinsInfo = async () => {
+      const response = await API.get("cryptoapi", "/coins", true);
+      console.log(response);
+    };
+
+    getCoinsInfo();
+  }, []);
+
   return (
     <div>
       <h1>Hello from AWS Amplify</h1>
